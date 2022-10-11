a0838e5abec035ddd265d1c5009976cc1af34fb8
Natã Muniz <natan.m25@gmail.com>
2022-10-11 07:41:21 -0300

    Adicionando forEach

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,23 @@
-const subtrair = document.querySelector('#subtrair')
-const somar = document.querySelector('#somar')
-const braco = document.querySelector('#braco')
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+ const controle = document.querySelectorAll(".controle-ajuste");
+
+ controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados  (evento.target.textContent)
+    })
+ });
 
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1
-})
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1
-})
+
+function manipulaDados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+};
 
