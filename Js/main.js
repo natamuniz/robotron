5526c30683639b8d3a79209c28553e5299a6c279
Natã Muniz <natan.m25@gmail.com>
2022-10-18 08:25:22 -0300

    Finalização do projeto robotron

--- Js/main.js
+++ Js/main.js
@@ -1,23 +1,69 @@
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]")
+const estatistica = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
  controle.forEach( (elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados  (evento.target.textContent, evento.target.parentNode)
+        manipulaDados  (evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatistica (evento.target.dataset.peca, evento.target.dataset.controle)
        
     })
- });
+ })
 
 
 
 
 function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector('.controle-contador');
-    if(operacao === "subtrair") {
-        peca.value = parseInt(braco.value) - 1
+    const peca = controle.querySelector("[data-contador]")
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
     } else {
-        peca.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
-};
+}
 
-//fazendo commit
+function atualizaEstatistica(peca, operacao) {
+    if(operacao === "+") {
+       estatistica.forEach( (elemento) => {
+         elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+       })
+     } else {
+       estatistica.forEach( (elemento) => {
+         elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+       })
+     }
+   }
+
 
